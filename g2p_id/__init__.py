51f813c293d1ab5358a4e80bf3babad6934c6e4d
w11wo <wilsonwong961@gmail.com>
2024-08-21 16:19:37 +1000

    Fixes https://github.com/bookbot-kids/g2p_id/issues/16

--- g2p_id/__init__.py
+++ g2p_id/__init__.py
@@ -19,5 +19,5 @@ from .lstm import LSTM
 from .bert import BERT
 from .text_processor import TextProcessor
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 __all__ = ["G2p", "LSTM", "BERT", "TextProcessor"]
