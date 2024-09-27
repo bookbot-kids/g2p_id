3ae05ade5a491c467b9a44b5dae4929d07b2fe80
w11wo <wilsonwong961@gmail.com>
2024-09-27 16:19:43 +1000

    Fix Pylint Issues

--- g2p_id/onnx_utils.py
+++ g2p_id/onnx_utils.py
@@ -1,3 +1,19 @@
+"""
+Copyright 2023 [PT BOOKBOT INDONESIA](https://bookbot.id/)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import onnxruntime as ort
 
 
@@ -12,6 +28,11 @@ class WrapInferenceSession:
         self.providers = providers
 
     def run(self, *args):
+        """Wrapper for ONNX InferenceSession run method.
+
+        Returns:
+            Any: Inference result.
+        """
         return self.sess.run(*args)
 
     def __getstate__(self):
